101d8cad382c875ad9533059888f5c40d05b8c42
Souhail M <souhail.merroun1996@gmail.com>
2023-05-07 18:34:27 +0000

    delete everything

--- cypress/e2e/sample/sample_spec.cy.js
+++
@@ -1,6 +0,0 @@
-describe("Sample Test", () => {
-  it("Visits the main page and checks the page title", () => {
-    cy.visit("https://example.com");
-    cy.title().should("include", "Example Domain");
-  });
-});
